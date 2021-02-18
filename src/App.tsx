e24ff0eb3606744e411638072b4fd687d23e639b
juandara <juandara2222@gmail.com>
2021-02-18 00:58:56 -0500

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App(props:AppProps) {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. TEST 1
         </p>
         <a
           className="App-link"
