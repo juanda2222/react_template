3c16d5e95bbdccd3290af54abb0926b1bfc788a1
juandara <juandara2222@gmail.com>
2021-07-27 18:38:40 -0500

    cloud run dockerfile addeed

--- setypPipeline.sh
+++ setypPipeline.sh
@@ -2,3 +2,4 @@
 
 gcloud projects create [PROJECT_ID] [--no-enable-cloud-apis] [--folder=FOLDER_ID] [--labels=[KEY=VALUE,…]] [--name=NAME] [--organization=ORGANIZATION_ID] [--set-as-default] [GCLOUD_WIDE_FLAG …]
 gcloud source repos create REPOSITORY_NAME [GCLOUD_WIDE_FLAG …]
+gcloud alpha builds triggers create COMMAND [GCLOUD_WIDE_FLAG …] -y
